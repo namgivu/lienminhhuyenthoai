39217a02a6a759d3b2319e43f25671b5fe51c137
kevin <dqtkien@gmail.com>
2020-05-31 15:54:02 +0700

    add body homepage content

--- src/pages/Home/HomeContent/HomeBody.jsx
+++ src/pages/Home/HomeContent/HomeBody.jsx
@@ -3,9 +3,16 @@ import React from "react";
 const HomeBody = () => {
   return (
     <main>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro eius
-      blanditiis dolor a rem reiciendis, facere dolores maxime neque accusamus
-      qui eaque quidem laboriosam obcaecati nulla enim tempore molestiae dolore?
+      <h1 className="home__heading">Cover your page.</h1>
+      <p className="home__p">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro eius
+        blanditiis dolor a rem reiciendis, facere dolores maxime neque accusamus
+        qui eaque quidem laboriosam obcaecati nulla enim tempore molestiae
+        dolore?
+      </p>
+      <p className="home__p">
+        <button className="btn btn-lg btn-secondary">Learn more</button>
+      </p>
     </main>
   );
 };
