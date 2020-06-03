a98db9cdf44d6313a9d6122f9d764aabe452f5ae
namgivu <namgivu@gmail.com>
2020-06-03 10:44:13 +0700

    github action test 01
    github action test 02
    
    github action test 03
    
    github action test 04
    
    github action test 05
    
    github action test 06
    
    github action test 07
    
    github action test 08

--- bin/deploy/buildrun-DEV.sh
+++ bin/deploy/buildrun-DEV.sh
@@ -2,5 +2,5 @@
 s=$BASH_SOURCE ; s=$(dirname "$s") ; s=$(cd "$s" && pwd) ; SCRIPT_HOME="$s"  # get SCRIPT_HOME=executed script's path, containing folder, cd & pwd to get container path
 a="$SCRIPT_HOME/../.."; a=$(cd "$a" && pwd); APP_HOME=$a; ROOT="$APP_HOME/../../.."; ROOT=$(cd "$ROOT" && pwd)
 
-
- b=deploy/DEV && git fetch && git checkout $b && git pull &&  git --no-pager log -1 && $APP_HOME/docker/build.sh && $APP_HOME/docker/run.sh
+  cd ~/lienminhhuyenthoai
+  b=deploy/DEV && git fetch && git checkout $b && git pull &&  git --no-pager log -1 && ./docker/build.sh && ./docker/run.sh
