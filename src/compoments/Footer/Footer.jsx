57b1954b1ff01f6947f1d660a38c28b4e5b3e6a7
kevin <dqtkien@gmail.com>
2020-06-05 22:26:14 +0700

    add version on footer

--- src/compoments/Footer/Footer.jsx
+++ src/compoments/Footer/Footer.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./footer.scss";
+import api from "compoments/Axios/api";
 
 const Footer = () => {
+  const [respondData, setRespondData] = useState("");
+
+  useEffect(() => {
+    const getDataFromHealthAPI = async () => {
+      try {
+        let r = await api.get("/health");
+        setRespondData(r.data.version);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getDataFromHealthAPI();
+  }, []);
+
   return (
     <footer className="home__footer mt-auto">
       Cover template for <a href="https://www.w3schools.com"> Bootstrap</a>, by
       @mdo.
+      <p>Version : {respondData} </p>
     </footer>
   );
 };
