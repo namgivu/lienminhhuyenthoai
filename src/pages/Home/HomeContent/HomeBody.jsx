447d5d71edcfb9ce5682e8d858a5d5815d8e7f7f
kevin <dqtkien@gmail.com>
2020-06-05 21:29:42 +0700

    fix typo

--- src/pages/Home/HomeContent/HomeBody.jsx
+++ src/pages/Home/HomeContent/HomeBody.jsx
@@ -2,16 +2,17 @@ import React, { useEffect, useState } from "react";
 import Axios from "axios";
 
 const HomeBody = () => {
-  const HEALTH_URL = "http://localhost:5000/health";  /*TODO Kien make this loaded via .env*/
+  const HEALTH_URL =
+    "http://localhost:5000/health"; /*TODO Kien make this loaded via .env*/
 
-  const [responeData, setResponeData] = useState("");  /*TODO Kien typo respond*/
+  const [respondData, setRespondData] = useState(""); /*TODO Kien typo respond*/
   //     stateValue   setState
 
   useEffect(() => {
     const getDataFromHealthAPI = async () => {
       try {
         let r = await Axios.get(HEALTH_URL);
-        setResponeData(r.data.message);
+        setRespondData(r.data.message);
       } catch (error) {
         console.log(error);
       }
@@ -23,7 +24,7 @@ const HomeBody = () => {
     <main>
       <h1 className="home__heading">Cover your page.</h1>
       <p className="home__p"></p>
-      <p>Message from Health API: {responeData} </p>
+      <p>Message from Health API: {respondData} </p>
       <p className="home__p">
         <button className="btn btn-lg btn-secondary">Learn more</button>
       </p>
